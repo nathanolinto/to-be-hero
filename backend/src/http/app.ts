244f03687fc02b2a1dbfe9e061a45e6c5986bb44
Nathan Olinto <nathanolinto@gmail.com>
2022-07-31 17:45:55 -0300

    added connection mongo

--- backend/src/http/app.ts
+++ backend/src/http/app.ts
@@ -4,6 +4,11 @@ import express, { Request, Response, NextFunction } from "express";
 import { AppError } from "../erros/AppErros";
 import { router } from "./routes";
 import "../container";
+import { connectDb } from "../services/mongoose";
+
+connectDb(
+  "mongodb://root:mobochatbot-mongo@localhost:27017/to_be_hero?authSource=admin"
+);
 
 const app = express();
 app.use(express.json());
